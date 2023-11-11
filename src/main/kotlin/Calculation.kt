f0e5657e04cacec5f0008824dd5dfb4c56f6d373
Yodahe-M <104659062+Yodahe-M@users.noreply.github.com>
2023-11-11 12:41:24 -0700

    basic calculator

---
+++ src/main/kotlin/Calculation.kt
@@ -0,0 +1,93 @@
+fun main(args: Array<String>) {
+
+    println("Welcome to the calculation game")
+
+    // User input
+    print("Enter what calculation you want to perform: '+', '-', '/', '*': ")
+    val sign = readLine()
+
+    if (sign == "+") {
+        // Reading an input
+        print("Enter the first number: ")
+        val num1Str = readLine()
+
+        print("Enter the second number: ")
+        val num2Str = readLine()
+
+        // Convert strings to floats
+        val num1 = num1Str?.toIntOrNull() ?: 0
+        val num2 = num2Str?.toIntOrNull() ?: 0
+        val add = addition(num1, num2)
+        println(add)
+
+    }
+
+    if (sign == "-") {
+        print("Enter the first number: ")
+        val num1Str = readLine()
+
+        print("Enter the second number: ")
+        val num2Str = readLine()
+
+        // Convert strings to int
+        val num1 = num1Str?.toIntOrNull() ?: 0
+        val num2 = num2Str?.toIntOrNull() ?: 0
+        val subtract = subtraction(num1, num2)
+        println(subtract)
+
+    }
+
+
+    if (sign == "/") {
+        print("Enter the first number: ")
+        val num1Str = readLine()
+
+        print("Enter the second number: ")
+        val num2Str = readLine()
+
+        // Convert strings to int
+        val num1 = num1Str?.toIntOrNull() ?: 0
+        val num2 = num2Str?.toIntOrNull() ?: 0
+        val divide = division(num1, num2)
+        println(divide)
+
+    }
+
+    if (sign == "*") {
+        print("Enter the first number: ")
+        val num1Str = readLine()
+
+        print("Enter the second number: ")
+        val num2Str = readLine()
+
+        // Convert strings to int
+        val num1 = num1Str?.toIntOrNull() ?: 0
+        val num2 = num2Str?.toIntOrNull() ?: 0
+        val multiply = multiplication(num1, num2)
+        println(multiply)
+
+    }
+
+
+
+}
+
+fun addition( firstNum: Int, secondNum: Int): Int{
+    return firstNum + secondNum
+}
+
+fun subtraction( firstNum: Int, secondNum: Int): Int{
+    return firstNum - secondNum
+}
+
+fun multiplication( firstNum: Int, secondNum: Int): Int{
+    return firstNum * secondNum
+}
+
+fun division( firstNum: Int, secondNum: Int): Int{
+    if (secondNum == 0){
+        println("Error: Division by zero is not allowed. Please try again")
+        return -1
+    }
+    return firstNum / secondNum
+}
---
+++ src/main/kotlin/README.md
@@ -0,0 +1,17 @@
+# overview
+
+This is a simple, basic calculator that performs the four fundamental calculations: addiction, subtraction, multiplication, and division.
+I developed this application using the Kotlin language. The purpose of this calculator is to assist users in performing arithmetic operations
+by allowing them to choose the desired operation and input two numbers for the calculation.
+Once the user enters the numbers, the application provides the result of the selected operation. 
+Additionally, there is error handling in place to manage situations where a user attempts to divide a number by zero, which is an invalid operation.
+
+You can watch a demo of my software here: [Software Demo Video](https://youtu.be/ZE-FSasmE1s)
+
+# Development Environment
+* IntelliJ IDEA
+* Kotlin 1.9.0
+* Git / GitHub
+
+# Useful Websites
+[Udemy Course](https://gale.udemy.com/course/kotlin-course/learn/lecture/4061240#overview)
